9ca24c08e244968c496aee1365c4f4020b2d823a
jessica draper <j.b.draper@hotmail.com>
2018-04-09 15:54:10 +1200

    Solution to first kata

--- april/9-april-2018.js
+++ april/9-april-2018.js
@@ -1,2 +1,5 @@
 // Kata - Codewars - return a string in reversed order with only alphabetical characters
 
+function reverseLetter(str) {
+  return str.replace(/[^a-z]/g, "").split("").reverse().join("")
+}
